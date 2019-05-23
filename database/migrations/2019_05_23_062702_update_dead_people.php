47c516cf9bf38f5bf2d13f295acd5bffd6740d1a
Dragos Plesca <dragos.plesca@gmail.com>
2019-05-23 09:34:12 +0300

    update

--- database/migrations/2019_05_23_062702_update_dead_people.php
+++ database/migrations/2019_05_23_062702_update_dead_people.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use App\Character;
 
 class UpdateDeadPeople extends Migration
 {
